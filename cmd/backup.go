1401039f09b510b8304a40b22b74f495ab8ce2e2
Julian Torres <julian.torres@mongodb.com>
2020-05-18 02:09:28 -0400

    backup for the brewfile, that's neat

--- cmd/backup.go
+++ cmd/backup.go
@@ -2,14 +2,17 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // backupCmd represents the backup command
 var backupCmd = &cobra.Command{
 	Use:   "backup",
-	Short: "A brief description of your command",
+	Short: "Backup various aspects of your dotfiles.",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -18,7 +21,12 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("backup called")
-
+		// TODO: This works, but I need to look into a better logging tool.
+		brewBackupCmd := exec.Command("brew", "bundle", "dump", "--file", viper.Get("dotfiles.backup").(string), "--force")
+		if err := brewBackupCmd.Run(); err == nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	},
 }
 
